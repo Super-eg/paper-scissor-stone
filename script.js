2d3cfb937d12f4d810b6f6c1393fd8aa191f220c
Super <71580764+Super-eg@users.noreply.github.com>
2021-09-25 10:26:24 +0800

    renew the file
    I replace the "addEventListner" in JF file with "onclick" in HTML.
    It will be more satisfying.

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-const paper = document.getElementById("paper");
-const scissor = document.getElementById("scissor");
-const stone = document.getElementById("stone");
-const show = document.getElementById('show');
+const show = document.getElementById("show");
 const enemy = document.getElementById("enemy");
 const me = document.getElementById("me");
-const enemyType = [ 'paper', 'scissor', 'stone' ];
+const enemyType = [ "Paper", "Scissor", "Stone" ];
 const enemyNumber = [ 1, 3, 9 ];
 const myNumber = [ 1, 2, 4 ];
 
-const random = () => Math.floor( Math.random() * 3 )
+const getRandomNumber = () => Math.floor( Math.random() * 3 )
 
-const doAllThing = ( playerNum, playerType ) => {
-	randomNumber = random();
+const doAllThings = ( playerNum, playerType ) => {
+	randomNumber = getRandomNumber();
 	judge( randomNumber, playerNum );
 	showType( randomNumber, playerType);
 }
@@ -19,15 +16,15 @@ const doAllThing = ( playerNum, playerType ) => {
 const judge = ( enemyNum, meNum ) => { showText( enemyNumber[enemyNum] * myNumber[meNum]) }
 
 const showText = ( ans ) => {
-	switch(ans) {
+	switch ( ans ) {
  	 case 2: case 9: case 12:
-    show.innerText = 'YOU WIN';
+    show.innerText = "YOU WIN";
     	break;
  	 case 3: case 4: case 18:
-   	show.innerText = 'you lose';
+   	show.innerText = "you lose";
    		break;
    case 1: case 6: case 36:
-   	show.innerText = 'TIE';
+   	show.innerText = "TIE";
   	 	break;
 	}
 }
@@ -36,7 +33,3 @@ const showType = ( enemyNum, meText ) => {
 	enemy.innerText = enemyType[enemyNum];
 	me.innerText = meText;
 }
-
-paper.addEventListener( "click", () => doAllThing( 0, 'paper' ));
-scissor.addEventListener( "click", () => doAllThing( 1, 'scirssor' ));
-stone.addEventListener( "click", () => doAllThing( 2 ,'stone' ));
